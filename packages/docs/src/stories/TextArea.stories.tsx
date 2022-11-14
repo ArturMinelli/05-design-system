238e5d85f57e69f2dd5d796552a45c08ffa3b35a
Artur Minelli Peixoto <arturminelli@gmail.com>
2022-11-14 12:51:17 -0300

    Componentes do Design System - Componente: TextArea

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, TextArea, TextAreaProps, Text } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/TextArea',
+  component: TextArea,
+  args: {
+    disabled: false,
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box as="label" css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}>
+          <Text>Observations</Text>
+          {Story()}
+        </Box>
+      )
+    }
+  ]
+} as Meta<TextAreaProps>
+
+export const Primary: StoryObj<TextAreaProps> = {
+  args: {
+    placeholder: "Add any observations..."
+  }
+}
